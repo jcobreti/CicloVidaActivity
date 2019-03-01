3ca2d6e8a80ea0e885dd8c003506519906c4192c
Julian <jnavamuel@gmail.com>
2019-03-01 12:26:04 +0100

    Initial commit

---
+++ app/src/main/java/android/edu/ciclovidaactivity/MainActivity.java
@@ -0,0 +1,19 @@
+package android.edu.ciclovidaactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+
+    public void cerrar(View view) {
+        finish();
+    }
+}
